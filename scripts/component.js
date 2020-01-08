28aab11693f36c6a08bc5b072afbddfe4ccb68ec
Steve Brownlee <chortlehoort@gmail.com>
2020-01-08 08:50:29 -0600

    Now with sections for each type of info

--- scripts/component.js
+++ scripts/component.js
@@ -4,12 +4,51 @@ let count = 1
 const canvas = document.querySelector('.canvas')
 
 export const makeComponent = () => {
-    const card = document.createElement("section")
 
-    card.textContent = "Component"
+    const header = document.createElement("header")
+    header.className = "component__header"
+    header.textContent = "Component"
+    header.contentEditable = true
+    header.addEventListener("click", function (e) {
+        e.target.textContent = ""
+        e.target.classList.add("edited")
+    })
+    header.addEventListener("keypress", function (e) {
+        console.log(e.keyCode)
+        e.target.classList.add("edited")
+        if (e.keyCode === 13) {
+            e.stopPropagation()
+            e.preventDefault()
+            e.returnValue = false
+            return false
+        }
+    })
+
+    const description = document.createElement("div")
+    description.className = "component__description"
+    description.textContent = "Responsibility"
+    description.contentEditable = true
+
+    const publish = document.createElement("div")
+    publish.className = "component__publish"
+    publish.textContent = "Publishes"
+    publish.contentEditable = true
+
+    const subscribe = document.createElement("div")
+    subscribe.className = "component__subscribe"
+    subscribe.textContent = "Subscribes"
+    subscribe.contentEditable = true
+
+    const card = document.createElement("section")
     card.classList = `component component--high`
     card.draggable = true
     card.id = `component--1`
+    card.addEventListener("click", e => e.target.focus())
+
+    card.appendChild(header)
+    card.appendChild(description)
+    card.appendChild(publish)
+    card.appendChild(subscribe)
 
     canvas.appendChild(card)
     count++
--- scripts/drag.js
+++ scripts/drag.js
@@ -1,33 +1,36 @@
+const canvas = document.querySelector('.canvas')
+
 export const drag = (elmnt) => {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0
 
     if (document.getElementById(elmnt.id + "header")) {
         // if present, the header is where you move the DIV from:
-        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown
     } else {
         // otherwise, move the DIV from anywhere inside the DIV:
-        elmnt.onmousedown = dragMouseDown;
+        elmnt.onmousedown = dragMouseDown
     }
 
     function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
+        e = e || window.event
+        e.preventDefault()
         // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
+        pos3 = e.clientX
+        pos4 = e.clientY
+        document.onmouseup = closeDragElement
         // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
+        document.onmousemove = elementDrag
+
     }
 
     function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
+        e = e || window.event
+        e.preventDefault()
 
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
+        pos1 = pos3 - e.clientX
+        pos2 = pos4 - e.clientY
+        pos3 = e.clientX
+        pos4 = e.clientY
 
 
         console.log(elmnt.offsetLeft + elmnt.offsetWidth, elmnt.parentNode.offsetWidth)
@@ -49,7 +52,7 @@ export const drag = (elmnt) => {
 
 
         if (elmnt.offsetLeft >= 20 && currentLeft <= parentWidth) {
-            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px"
         } else {
             if (elmnt.offsetLeft <= 20) {
                 elmnt.style.left = "20px"
@@ -63,8 +66,11 @@ export const drag = (elmnt) => {
     }
 
     function closeDragElement (e) {
-        document.onmouseup = null;
-        document.onmousemove = null;
+        console.log("closeDragElement")
+        document.onmouseup = null
+        document.onmousemove = null
+        canvas.focus()
+        canvas.blur()
     }
 }
 
